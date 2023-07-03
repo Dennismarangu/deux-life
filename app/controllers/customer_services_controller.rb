999786f3ccb14b4f3fe6c8abc052645149fb125f
Yazeed <dee3ayahmed25@gmail.com>
2023-07-03 22:44:30 +0300

    added rescue_from block

--- app/controllers/customer_services_controller.rb
+++ app/controllers/customer_services_controller.rb
@@ -1,2 +1,8 @@
 class CustomerServicesController < ApplicationController
+
+  before_action :set_customer_service, only: [:show, :update, :destroy]
+
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+
 end
