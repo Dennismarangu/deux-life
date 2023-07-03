d17d2bb0bf3e5bc9421f50382e6827b25fe84b97
Yazeed <dee3ayahmed25@gmail.com>
2023-07-03 22:58:31 +0300

    added post request to create

--- app/controllers/customer_services_controller.rb
+++ app/controllers/customer_services_controller.rb
@@ -16,4 +16,21 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     render json: @customer_service
   end
 
+   # POST /customer_services
+  def create
+    @customer_service = CustomerService.new(customer_service_params)
+
+    # Fetch the service price based on service_id from Services table
+    service_price = 10.0 
+
+    # Calculate total_price
+    @customer_service.total_price = params[:quantity] * service_price
+
+    if @customer_service.save
+      render json: @customer_service, status: :created
+    else
+      render json: @customer_service.errors, status: :unprocessable_entity
+    end
+  end
+
 end
