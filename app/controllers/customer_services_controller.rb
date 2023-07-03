148283bc20e302bcafffed1faccc75c72c6a2155
Yazeed <dee3ayahmed25@gmail.com>
2023-07-03 22:49:22 +0300

    added get request

--- app/controllers/customer_services_controller.rb
+++ app/controllers/customer_services_controller.rb
@@ -5,4 +5,10 @@ class CustomerServicesController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
 
+   # GET /customer_services
+  def index
+    customer_services = CustomerService.all
+    render json: customer_services
+  end
+
 end
