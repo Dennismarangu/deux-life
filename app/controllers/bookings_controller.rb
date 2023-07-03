d4d3f02b07102d6917ece18d126a0056ec013b18
Carlton Kibe <Virgotex15@gmail.com>
2023-07-03 21:38:43 +0300

    updating bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,4 +7,23 @@ class BookingsController < ApplicationController
   def show
     render json: @booking
   end
+
+  def update
+    if @booking.update(booking_params)
+      render json: @booking
+    else
+      render json: @booking.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_name, :check_in_date, :check_out_date)
+  end
+end
 end
