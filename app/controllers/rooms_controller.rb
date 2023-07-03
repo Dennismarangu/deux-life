56f6e6cd31a2e0679b1b6742eb823ed87c1d51ca
Carlton Kibe <Virgotex15@gmail.com>
2023-07-03 13:40:46 +0300

    update rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,3 +6,8 @@ def show
   @room = Room.find(params[:id])
   render json: @room
 end
+def update
+  @room = Room.find(params[:id])
+  @room.update(is_booked: params[:is_booked])
+  render json: @room
+end
