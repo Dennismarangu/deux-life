1c9b30dae5d7480967f1178fb3f0ed6eb5c612df
Alex Njagi <1992anjagi@gmail.com>
2023-07-03 17:34:40 +0300

    Cleaned up the code by removing unnecessary additions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,7 @@
-require 'faker'
-
-7.times do
-  customer = Customer.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    phone: Faker::PhoneNumber.phone_number,
-    password: 'password',
-    password_confirmation: 'password'
-  )
-  puts "Created customer with email: #{customer.email}"
-end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
