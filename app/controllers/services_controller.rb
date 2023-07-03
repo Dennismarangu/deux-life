90c01b4e9d6a9f76b43b5a5c3987331c848565a1
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-03 07:39:21 +0300

    Initial Routes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,1 +1,12 @@
+class ServicesController < ApplicationController
 
+  def index
+    @services = Service.all
+    # Add any additional logic needed
+  end
+
+    def show
+    @service = Service.find(params[:id])
+    # Add any additional logic needed
+  end
+end
