4b774e05d4d6a80060cec29e9b2e07056b42fe8a
Carlton Kibe <Virgotex15@gmail.com>
2023-07-03 21:59:42 +0300

    Finished bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/services', to: 'services#index'
   get '/services/:id', to: 'services#show'
   resources :rooms, only: [:index, :show, :update]
+  resources :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_03_103026) do
+ActiveRecord::Schema.define(version: 2023_07_03_183120) do
 
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_name"
+    t.integer "room_id"
+    t.date "check_in_date"
+    t.date "check_out_date"
   end
 
   create_table "customer_services", force: :cascade do |t|
