3891f7fedb82e3bc7425339b03d063e98c557dcc
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-09 12:13:40 +0300

    Updated code

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.2'
+ruby '>= 2.7.4'
+gem 'rack-cors'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.7', '>= 6.1.7.3'
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,17 +1,17 @@
 class ServicesController < ApplicationController
-
+  
   def index
     @services = Service.all
     render json: @services, status: :ok
   end
 
-    def show
+  def show
     @service = Service.find(params[:id])
 
     if @service
-      render json: @service: [:id, :service_name, :service_cost, :service_description, :head_of_service]
+      render json: @service, only: [:id, :service_name, :service_cost, :service_description, :head_of_service]
     else
-      render json: {error: "Service not found"}, status: :not_found
+      render json: { error: "Service not found" }, status: :not_found
     end
   end
 end
